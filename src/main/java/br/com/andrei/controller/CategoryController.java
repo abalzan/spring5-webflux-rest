e4b705d8cd20e5b15f5ef8bf0954cc2011c78b36
Andrei <andreibalzan@gmail.com>
2018-01-22 15:38:49 +0100

    removing vendor repository put in this class by mistake.

--- src/main/java/br/com/andrei/controller/CategoryController.java
+++ src/main/java/br/com/andrei/controller/CategoryController.java
@@ -1,25 +1,22 @@
 package br.com.andrei.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.andrei.domain.Category;
 import br.com.andrei.repository.CategoryRepository;
-import br.com.andrei.repository.VendorRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class CategoryController {
 
-	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	@Autowired
-	private VendorRepository vendorRepository;
-	
+	public CategoryController(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
 
 	@GetMapping("/api/v1/categories")
 	public Flux<Category> list(){
